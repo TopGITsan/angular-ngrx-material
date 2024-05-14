f8a9f42d1569a18ad223eb1279ee691aa90ab875
TopGitSan <c.toparcean@gmail.com>
2024-05-14 16:33:19 +0200

    refactor(shell): :recycle: get signal element ref, use reactive logic for typeahead

--- src/app/layout/shell/shell.component.ts
+++ src/app/layout/shell/shell.component.ts
@@ -1,16 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, DestroyRef, ElementRef, Signal, viewChild } from '@angular/core';
 // import { MatFormFieldModule } from "@angular/material/form-field";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { RouterModule } from '@angular/router';
-import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, fromEvent, map } from 'rxjs';
 import { BookmarkService } from '../../shared/services/bookmark/bookmark.service';
 import { GlobalStoreFacadeService } from '../../store/global.store.facade';
-
-
 
 @Component({
   selector: 'app-shell',
@@ -20,12 +19,27 @@ import { GlobalStoreFacadeService } from '../../store/global.store.facade';
   styleUrl: './shell.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ShellComponent {
-  constructor(public readonly bookmarkService: BookmarkService, private readonly store: GlobalStoreFacadeService) { }
+export class ShellComponent implements AfterViewInit {
 
-  onFilterChange(inputEvent: Event): void {
-    const target: HTMLInputElement = (inputEvent.target) as HTMLInputElement;
-    this.store.onFilterChange(target.value ?? '');
+  searchFilterTextSignal: Signal<ElementRef | undefined> = viewChild.required<ElementRef<HTMLInputElement>>('inputFilterText');
+  constructor(public readonly bookmarkService: BookmarkService, private readonly store: GlobalStoreFacadeService, private readonly destroyRef: DestroyRef) { }
+  ngAfterViewInit(): void {
+    const searchFilterTextElement: HTMLInputElement | undefined = this.searchFilterTextSignal()?.nativeElement;
+    if (searchFilterTextElement) {
+      fromEvent(searchFilterTextElement, 'input').pipe(
+        takeUntilDestroyed(this.destroyRef),
+        debounceTime(450),
+        map((event) => ((event.target) as HTMLInputElement).value),
+        distinctUntilChanged(),
+      ).subscribe({
+        next: (value) => this.onFilterChange(value),
+        error: () => console.log(`[${ShellComponent.name}] something whent wrong`)
+      })
+    }
+  }
+
+  onFilterChange(filterText: string): void {
+    this.store.onFilterChange(filterText ?? '');
   }
 
 }
