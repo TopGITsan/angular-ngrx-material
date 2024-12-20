8546d8324e20374c35cf19c03ddf95dd7d493347
TopGitSan <c.toparcean@gmail.com>
2024-12-20 12:49:54 +0100

    feat(bookmark-service): :sparkles: add a searchBookmark function to simulate an api request with pagination

--- src/app/shared/services/bookmark/bookmark.service.ts
+++ src/app/shared/services/bookmark/bookmark.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, delay, throwError } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
 
 export interface Bookmark {
   id: number;
@@ -9,29 +9,72 @@ export interface Bookmark {
   created: Date;
 }
 
+export interface BookmarkApiResponse {
+  searchTerm: string;
+  page: number;
+  pages: number;
+  perPage: number;
+  total: number;
+  bookmarks: Bookmark[];
+  status: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class BookmarkService {
-  allBookmarks: Bookmark[] = [];
-  editBookmark: Bookmark | undefined;
-  filterText: string = '';
-
-  constructor(private readonly http: HttpClient) {}
+  private readonly http = inject(HttpClient);
+  private readonly url = 'api/bookmarks';
 
   public getAll(): Observable<Bookmark[]> {
-    return this.http.get<Bookmark[]>('api/bookmarks');
+    return this.http.get<Bookmark[]>(this.url);
+  }
+
+  public searchBookmark(
+    searchTerm: string,
+    page: number,
+  ): Observable<BookmarkApiResponse | { error: string }> {
+    const perPage = 20;
+    return this.getAll().pipe(
+      map((bookmarks: Bookmark[]) => {
+        if (!searchTerm) {
+          return {
+            page,
+            pages: bookmarks.length / perPage,
+            perPage,
+            total: bookmarks.length,
+            bookmarks: bookmarks.slice(perPage * page, perPage),
+            status: '200',
+            searchTerm,
+          };
+        }
+        const searchedBookmarks = bookmarks.filter(
+          (b: Bookmark) =>
+            !!b.name.toLowerCase().includes(searchTerm.toLowerCase()),
+        );
+        return {
+          page,
+          pages: searchedBookmarks.length / perPage,
+          perPage,
+          total: searchedBookmarks.length,
+          bookmarks: searchedBookmarks.slice(perPage * page, perPage),
+          status: '200',
+          searchTerm,
+        };
+      }),
+      catchError((error) => of({ error })),
+    );
   }
 
   public getById(bookmarkId: number): Observable<Bookmark> {
-    return this.http.get<Bookmark>(`api/bookmarks/${bookmarkId}`);
+    return this.http.get<Bookmark>(`${this.url}/${bookmarkId}`);
   }
 
   public save(bookmark: Bookmark): Observable<Bookmark> {
-    return this.http.post<Bookmark>(`api/bookmarks`, bookmark);
+    return this.http.post<Bookmark>(`${this.url}`, bookmark);
   }
 
   public update(bookmark: Bookmark): Observable<Bookmark> {
-    return this.http.put<Bookmark>(`api/bookmarks`, bookmark);
+    return this.http.put<Bookmark>(`${this.url}`, bookmark);
   }
 }
