0b734320120213fa7d7848d9a4d861b2347b31a3
TopGitSan <c.toparcean@gmail.com>
2024-12-20 12:43:00 +0100

    feat(shared/utils): :sparkles: create util functions for getRandomInteger and removeHttp from a string

--- src/app/shared/util.ts
+++ src/app/shared/util.ts
@@ -1,13 +1,19 @@
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
-export class Util {
-
-}
+export class Util {}
 
 export function isToday(created: Date) {
   return dayjs(created).isSame(dayjs().startOf('day'), 'd');
 }
 
 export function isYesterday(created: Date) {
-  return dayjs(created).isSame(dayjs().subtract(1, 'day').startOf('day'), 'd')
+  return dayjs(created).isSame(dayjs().subtract(1, 'day').startOf('day'), 'd');
+}
+
+export function getRandomInt(max: number): number {
+  return Math.floor(Math.random() * max);
+}
+
+export function removeHttp(url: string): string {
+  return url.replace(/^https?:\/\//, '');
 }
