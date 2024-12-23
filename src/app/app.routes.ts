00fdbc744cd66a2a340bd01893547dbcf3f1ab73
TopGitSan <c.toparcean@gmail.com>
2024-12-23 12:10:56 +0100

    feat(app.routes): :triangular_flag_on_post: add the bookmarks-list using signals to the route signal-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,26 +2,44 @@ import { Routes } from '@angular/router';
 import { editGuard } from './guards/edit.guard';
 import { listGuard } from './guards/list.guard';
 import { ShellComponent } from './layout/shell/shell.component';
-
+import { SignalListComponent } from './layout/shell/signal-list/signal-list.component';
 
 export const routes: Routes = [
   {
-    path: '', component: ShellComponent, children: [
+    path: '',
+    component: ShellComponent,
+    children: [
       {
         path: 'create',
-        loadComponent: () => import('./layout/containers/create-bookmarks/create-bookmarks.component').then(m => m.CreateBookmarksComponent)
+        loadComponent: () =>
+          import(
+            './layout/containers/create-bookmarks/create-bookmarks.component'
+          ).then((m) => m.CreateBookmarksComponent),
       },
       {
         path: 'list',
-        loadComponent: () => import('./layout/containers/list-bookmarks/list-bookmarks.component').then(m => m.ListBookmarksComponent),
+        loadComponent: () =>
+          import(
+            './layout/containers/list-bookmarks/list-bookmarks.component'
+          ).then((m) => m.ListBookmarksComponent),
         canActivate: [listGuard],
       },
       {
         path: 'edit/:bookmarkId',
-        loadComponent: () => import('./layout/containers/edit-bookmark/edit-bookmark.component').then(m => m.EditBookmarkComponent),
-        canActivate: [editGuard]
+        loadComponent: () =>
+          import(
+            './layout/containers/edit-bookmark/edit-bookmark.component'
+          ).then((m) => m.EditBookmarkComponent),
+        canActivate: [editGuard],
       },
-      { path: '', redirectTo: '/list', pathMatch: 'full' }
-    ]
-  }
+      { path: '', redirectTo: '/list', pathMatch: 'full' },
+    ],
+  },
+  {
+    path: 'signal-list',
+    loadComponent: () =>
+      import('./layout/shell/signal-list/signal-list.component').then(
+        (m) => m.SignalListComponent,
+      ),
+  },
 ];
