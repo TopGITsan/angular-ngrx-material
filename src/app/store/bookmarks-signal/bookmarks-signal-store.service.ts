f70f95e56eed8b40e3bbc253590727f6f30ff78e
TopGitSan <c.toparcean@gmail.com>
2024-12-24 12:02:40 +0100

    docs(bookmarks-signal-store): :memo: add documentation to the store's connection observable, derived state

--- src/app/store/bookmarks-signal/bookmarks-signal-store.service.ts
+++ src/app/store/bookmarks-signal/bookmarks-signal-store.service.ts
@@ -136,13 +136,27 @@ export class BookmarksSignalStoreService {
     this.searchResultsChange$,
     this.pageChange$,
   );
-
+  /**
+   * derived state with signals
+   *
+   * create a connection between the observable and the signal
+   *
+   * - automatic cleanup (Observables can automatically clean up the connections or listeners when the user goes to other pages and all subscribers have unsubscribed.)
+   * - reset and re-fetch (Observables can automatically reset state when the user goes to other pages and all subscribers have unsubscribed, and automatically re-fetch data when the user returns to a page that needs it)
+   * - automatic cancel (Observables can automatically cancel requests when the user goes to other pages and all subscribers have unsubscribed)
+   * - automatic defer (Observables can represent data sources but wait for subscribers instead of prematurely consuming them)
+   */
   connection$ = merge(this.newState$, NEVER).pipe(
+    // set the signal state to the state that came from the observable
     tap((state) => {
       console.log('new state: ', { state });
       this.state.set(state);
     }),
+    // when all the subscribers are done set it back to the initial state
+    // sometimes the source can self complete and that will trigger finalize to run
+    // we want finalize to happen when all the subscribers unsubscribe (trick: merge the source observable with a NEVER observable)
     finalize(() => this.state.set(defaultState)),
+    // it should only do this once no matter how many subscribers has
     share(),
   );
 }
